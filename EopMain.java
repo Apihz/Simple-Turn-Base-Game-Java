3f3f74e4c9675cb689e9943b48d100ea01707a9c
Apihz <hafyz74@gmail.com>
2023-12-06 20:20:40 +0800

    Committing local changes to EopMain.java

--- EopMain.java
+++ EopMain.java
@@ -1,7 +1,7 @@
 import java.util.Random; 
 import java.util.Scanner;
 
-public class Test2 {
+public class EopMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
